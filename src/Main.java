bad9f46b07eb74b8cfbcc0cc2d65583e3637c0f0
vladimir.ovodenko <vladimir.ovodenko@gamesys.com>
2022-11-11 19:50:47 +0200

    guthub

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world and git!");
         System.out.println("Hello world and git!");
 
+        System.out.println("Hello world and github!");
     }
 }
